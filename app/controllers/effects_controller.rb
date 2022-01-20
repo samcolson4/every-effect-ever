4fd145634fefd00bfe940de4886acc9ee6f679fa
Sam Colson <samcolson4@gmail.com>
2022-01-20 23:50:39 +0000

    Display random effect

--- app/controllers/effects_controller.rb
+++ app/controllers/effects_controller.rb
@@ -1,5 +1,14 @@
 class EffectsController < ApplicationController
   def index
     @effects = Effect.all
+
+    @four_random = []
+    1.times {
+      pedal_id = rand(@effects.length + 1)
+      if pedal_id == 0
+        pedal_id += 1
+      end
+      @four_random.append(Effect.find_by(id: pedal_id))
+    }
   end
 end
--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -1,2 +1,6 @@
 module BrandsHelper
+  def findBrand(brand_id)
+    brand = Brand.find_by(id: brand_id)
+    return brand
+  end
 end
