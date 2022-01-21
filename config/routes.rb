ef8734f3b9548eb8c1f4373c4dd9a1eaae50b79f
Sam Colson <samcolson4@gmail.com>
2022-01-21 22:05:15 +0000

    Add about

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/effects', to: "effects#index"
   get '/brands', to: "brands#index"
   get '/variants', to: "variants#index"
+  get '/about', to: "about#index"
 end
