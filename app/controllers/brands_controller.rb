c8b809cfe100d69f026b1d80c88747a9882afcfe
Sam Colson <samcolson4@gmail.com>
2022-01-29 14:51:08 +0000

    Brand has to have name to save

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -5,12 +5,23 @@ class BrandsController < ApplicationController
 
   def create
     @brand = Brand.new(brand_params)
-    @brand.save
-    redirect_to "/contribute/thank-you"
+
+    if hasName(@brand)
+      @brand.save
+      redirect_to "/contribute/thank-you"
+    else
+      redirect_to "/brands/contribute"
+      # TODO add flash message re: required
+    end
+
   end
 
   private
     def brand_params
       params.require(:brand).permit(:name, :website_link, :logo_image_link, :year_founded, :country_founded, :area_founded, :parent_company, :notes)
+    end
+
+    def hasName(object)
+      object.name != ""
     end
 end
