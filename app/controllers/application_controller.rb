dd2c84c1240c6ef1552ac8aa73290df723f270bc
Sam Colson <samcolson4@gmail.com>
2022-01-30 02:41:20 +0000

    WIP: Add version form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :initialize_brand, :initialize_effect
+  before_action :initialize_brand, :initialize_effect, :initialize_version
+
   def initialize_brand
     @brand = Brand.new
   end
 
   def initialize_effect
     @effect = Effect.new
+  end
+
+  def initialize_version
+    @version = Version.new
   end
 end
